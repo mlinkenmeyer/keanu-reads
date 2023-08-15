9979669d94b0c6b4ccca80bdd57916254d9b80bd
Rae Stanton <raestanton93@gmail.com>
2023-08-15 11:37:08 -0700

    Card template
    This adds a concept of a template card that isn't shown in the dom, but will only show book card elements that have data attached to them.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const bookList = document.querySelector("div");
-bookList.className = "books-of-the-month";
+const bookList = document.querySelector(".books-of-the-month");
 
 const searchBookByTitle = (title) => {
   fetch(`https://openlibrary.org/search.json?title=${title}`)
     .then((response) => response.json())
     .then((data) => {
       if (data.docs && data.docs.length) {
-        createBook(data.docs[0]); // Only use the first result
+        createBook(data.docs[0]);
       } else {
         console.log("No books found for title:", title);
       }
@@ -23,48 +22,78 @@ const fetchAuthor = (authorId) => {
       console.error("Failed to fetch author:", error);
     });
 };
+// FIX LATER, this will eventually be the description.
+// const fetchBookDescription = (isbn) => {
+//   return fetch(
+//     `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=details&format=json`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const details = extractDetailsFromResponse(data);
+//       if (details && details.description) {
+//         return details.description.value;
+//       } else {
+//         return "This book doesn't have a description.";
+//       }
+//     });
+// };
+
+// const extractDetailsFromResponse = (response) => {
+//   for (let topLevelKey in response) {
+//     if (
+//       response.hasOwnProperty(topLevelKey) &&
+//       response[topLevelKey].hasOwnProperty("details")
+//     ) {
+//       return response[topLevelKey].details;
+//     }
+//   }
+//   return null;
+// };
 
 const createBook = (book) => {
-  const bookName = document.createElement("ul");
-  bookName.className = "book-name";
-  bookName.textContent = book.title;
-  bookList.appendChild(bookName);
+  const templateCard = document.querySelector('.book-card.template');
+  const newCard = templateCard.cloneNode(true);
 
-  const bookCover = document.createElement("img");
-  bookCover.className = "book-cover";
-  bookCover.src = `http://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-  bookName.appendChild(bookCover);
+  newCard.classList.remove('template');
 
-  fetchBookDescription(book.isbn[0])
-    .then((description) => {
-      const bookDescription = document.createElement("p");
-      bookDescription.className = "book-description";
-      bookDescription.textContent = description;
-      bookName.appendChild(bookDescription);
-    })
-    .catch((error) => {
-      console.error("Error fetching book description:", error);
-    });
+  const cardTitle = newCard.querySelector('.card-title');
+  cardTitle.textContent = book.title;
+
+  const cardImage = newCard.querySelector('.card-img-top');
+  cardImage.src = `http://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+  cardImage.alt = book.title;
+
+  const cardText = newCard.querySelector('.author-text');
 
   if (book.author_key && book.author_key.length) {
-    // this takes in the first author of the books
     fetchAuthor(book.author_key[0]).then((authorData) => {
       if (authorData) {
-        const bookAuthor = document.createElement("ul");
-        bookAuthor.className = "book-author";
-        bookAuthor.textContent = authorData.name;
-        bookName.appendChild(bookAuthor);
+        cardText.textContent = authorData.name;
       }
     });
+  } else {
+    cardText.textContent = "Author info not available.";
   }
 
-  //Like Button Creation & toggler
-  const likeButton = document.createElement("button");
-  likeButton.className = "like-button";
+
+  // This will eventually be the book description in our JSON
+  // if (book.isbn && book.isbn.length) {
+  //   fetchBookDescription(book.isbn[0])
+  //     .then((description) => {
+  //       const bookDescription = document.createElement("p");
+  //       bookDescription.className = "book-description";
+  //       bookDescription.textContent = description;
+  //       newCard.appendChild(bookDescription);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching book description:", error);
+  //     });
+  // }
+
+  // Like Button
+  const likeButton = newCard.querySelector("#like-button");
   likeButton.textContent = "Like book";
-  bookList.appendChild(likeButton);
-  likeButton.addEventListener("click", (e) => {
-    console.log(e);
+  likeButton.addEventListener("click", () => {
     if (likeButton.textContent === "Like book") {
       likeButton.textContent = "Unlike book";
     } else {
@@ -72,7 +101,10 @@ const createBook = (book) => {
     }
   });
 
-  // Month Dropdown Menu
+  bookList.appendChild(newCard);
+};
+
+const createMonthDropdown = () => {
   const months = ["August", "September", "October", "November", "December"];
   const filterByMonth = document.createElement("select");
   filterByMonth.id = "filter-by-month";
@@ -82,56 +114,27 @@ const createBook = (book) => {
   monthOptions.textContent = "Select month";
   filterByMonth.appendChild(monthOptions);
 
-  for (let i = 0; i < months.length; i++) {
+  for (let month of months) {
     const monthOption = document.createElement("option");
-    monthOption.value = months[i];
-    monthOption.textContent = months[i];
+    monthOption.value = month;
+    monthOption.textContent = month;
     filterByMonth.appendChild(monthOption);
   }
 };
 
-// this function grabs the data for our description
-
-const fetchBookDescription = (isbn) => {
-  return fetch(
-    `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=details&format=json`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const details = extractDetailsFromResponse(data);
-      if (details && details.description) {
-        return details.description.value;
-      } else {
-        return "This book doesn't have a description.";
-      }
-    });
-};
-// description data fetch continued
-const extractDetailsFromResponse = (response) => {
-  for (let topLevelKey in response) {
-    if (
-      response.hasOwnProperty(topLevelKey) &&
-      response[topLevelKey].hasOwnProperty("details")
-    ) {
-      return response[topLevelKey].details;
-    }
-  }
-  return null;
-};
-
-//begin fetches for db.json
+// Fetch books from db.json
 const fetchBooksFromDB = () => {
   fetch("http://localhost:3000/books")
     .then((response) => response.json())
     .then((data) => {
       if (data && data.length) {
-        for (let book of data) {
+        data.forEach((book) => {
           if (!book.image) {
             searchBookByTitle(book.title);
           } else {
             createBook(book);
           }
-        }
+        });
       } else {
         console.log("No books found in db.json");
       }
@@ -141,28 +144,32 @@ const fetchBooksFromDB = () => {
     });
 };
 
+// Fetch and display the books when the page loads
 fetchBooksFromDB();
 
+// Comment form
 const commentDiv = document.querySelector("#comment-section");
-
 const commentForm = document.querySelector("#comment-form");
+
 commentForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const formContent = {
     username: document.querySelector("#username").value,
     title: document.querySelector("#commentTitle").value,
     comment: document.querySelector("#newComment").value,
   };
-  console.log(formContent);
 
   const commentDetails = document.createElement("div");
   commentDetails.innerHTML = `
-  <p> Username: ${formContent.username} </p>
-  <p> Title: ${formContent.title} </p>
-  <p> Comment: ${formContent.comment} </p>
+    <p> Username: ${formContent.username} </p>
+    <p> Title: ${formContent.title} </p>
+    <p> Comment: ${formContent.comment} </p>
   `;
 
   commentDiv.appendChild(commentDetails);
-
   commentForm.reset();
 });
+
+// Create the month dropdown
+createMonthDropdown();
